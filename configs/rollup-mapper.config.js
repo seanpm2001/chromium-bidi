a9209f725d13b8473304678d5da9fbcc57a097e4
Alex <alexei.rudenko@gmail.com>
2022-11-28 21:33:51 +0100

    wip

---
+++ configs/rollup-mapper.config.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2021 Google LLC.
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from "rollup-plugin-dts";
+
+export default [
+    {
+        input: 'lib/cjs/bidiMapper/index.js',
+        output: {
+            file: 'lib/cjs-rolled/mapper.js',
+            sourcemap: true,
+            format: 'commonjs',
+            exports: 'auto'
+        },
+        plugins: [nodeResolve(), commonjs()],
+    },
+    {
+        input: "lib/cjs/bidiMapper/index.d.ts",
+        output: [{ file: "lib/cjs-rolled/mapper.d.ts", format: "es" }],
+        plugins: [dts()],
+    },
+];
---
+++ src/bidiMapper/index.ts
@@ -0,0 +1,6 @@
+export * from './commandProcessor';
+export * from './utils/bidiServer';
+export * from './utils/typeHelper';
+export * from '../cdp/index';
+export * from '../utils/transport';
+export * from './domains/events/EventManager';
